f7a350f2897542505435592cc82e000b22b037e2
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-12-13 23:49:08 +0700

    connect-database

--- src/server.js
+++ src/server.js
@@ -1,21 +1,39 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT ||6969 ;
-const hostname = process.env.HOST_NAME||'localhost';
+const port = process.env.PORT || 6969;
+const hostname = process.env.HOST_NAME || 'localhost';
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-
+//get the client
+const mysql = require('mysql2');
 //config template engine
 configViewEngine(app)
 
 // //config static files
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',webRoutes);
+app.use('/', webRoutes);
+
+//test connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit'
+})
+
+connection.query(
+  'SELECT * FROM Users u',
+  function (err, result, fields) {
+    console.log('>>>result = ', result);
+    console.log('fields = ', fields);
+  }
+)
 
 
-app.listen(port,hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
