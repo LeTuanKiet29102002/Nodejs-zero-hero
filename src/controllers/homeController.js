d9bc06ac2d22c6a3020c44337f9d191a16147c58
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-12-14 20:08:40 +0700

    Get-data-from-input

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -13,8 +13,18 @@ const getKimoon = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    console.log('check req json :', req.body);
-    res.send('create new users');
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+
+    connection.query(
+        `INSERT INTO Users (email, name, city) VALUES (?,?,?)`,
+        [email, name , city],
+        function(err, results){
+            console.log((results));
+            res.send('Created user successfully')
+        }
+    );
 }
 
 module.exports = { getHomePage, getKimoon ,postCreateUser};
